5eb3dd6cf9bf38537ea8a1f1e2c1bf1229f74215
Piotr Manczak <51323846+PiotrManczak39@users.noreply.github.com>
2019-09-21 19:26:05 +0100

    Looks like final before submition.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
-console.log('Hello World!');
 
 $(document).ready(function() {
        $("#lightgallery").lightGallery();
    });
+
+const searchBar = document.querySelector('.search-game');
+searchBar.addEventListener('keyup', function(e) {
+  const term = e.target.value.toLowerCase();
+  const images = document.getElementsByTagName("img");
+  Array.from(images).forEach(function(image){
+    const description = image.getAttribute('alt')
+    if (description.toLowerCase().indexOf(term) == -1) {
+      image.style.display = 'none';
+    } else {
+      image.style.display = 'block';
+    }
+  });
+});
